19058e56c6ace400d5d00c85b05b7bd0550e1e65
04735666214 <04735666214@JIPA-LABIN02-10.ifro.local>
2018-05-02 14:35:24 -0400

    tela de cadastro e menu principal terminado

--- 3a_Eduarda/src/br/edu/ifro/MenuPrincipalController.java
+++ 3a_Eduarda/src/br/edu/ifro/MenuPrincipalController.java
@@ -5,14 +5,18 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -20,10 +24,12 @@ import javafx.scene.control.TextField;
  */
 public class MenuPrincipalController implements Initializable {
 
+    @FXML
     private TextField txtNumero1;
     
+    private TextField txtNumero2;
     @FXML
-    private TextField txtNumero2,txtResultado ;
+    private TextField txtResultado;
     
     @FXML
     private Button btnSoma;  ;
@@ -45,5 +51,25 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void AbrirCadastroAluno(ActionEvent event) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+     Scene scene = new Scene(fxmlLoader.load(),900,682);
+            Stage stage = new Stage();
+    stage.setTitle("Cadastrar Aluno");
+    stage.setScene(scene);
+    stage.show();
+}
+catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void FecharProjeto(ActionEvent event) {
+    }
     
 }
